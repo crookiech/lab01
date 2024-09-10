6cfbf91a0273e200d825a796e64ed6867ca0e1c8
Valeria <timohinava2005@gmail.com>
2024-09-10 15:30:16 +0700

    pupupu

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,13 @@
-#include "lib.h"
-#include "version.h"
+// #include "lib.h"
+// #include "version.h"
 
-int version() {
-    return PROJECT_VERSION_PATCH;
+// int version() {
+//     return PROJECT_VERSION_PATCH;
+// }
+
+#include "version.h"
+#include "lib.h"
+
+std::string version() {
+    return "0.0.1"; // Верните версию
 }
